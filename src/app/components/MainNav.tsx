d0007dec43129dd812c66be54175aebc4daca458
Kristofer <kristofer@fmckl.se>
2023-09-07 20:22:10 +0200

    New logo

--- src/app/components/MainNav.tsx
+++ src/app/components/MainNav.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export function MainNav() {
   return (
-    <div className="w-full flex px-10 items-center justify-between">
+    <div className="w-full flex mt-3 px-7 items-center justify-between">
       <div className="flex gap-5 items-center">
         <div>
           <Link href="/">
-            <img src="/optimism.svg" alt="Optimism" />
+            <img src="/optimism-attestations.svg" alt="Optimism Attestations" />
           </Link>
         </div>
         <Link href="/1" className="hover:bg-theme-3 hover:bg-opacity-20 p-5">
