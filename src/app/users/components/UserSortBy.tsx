293a5f13da59fe218adf04ece5edf73089d694e7
Kristofer <kristofer@fmckl.se>
2023-09-07 20:37:13 +0200

    Sort dropdown styling

--- src/app/users/components/UserSortBy.tsx
+++ src/app/users/components/UserSortBy.tsx
@@ -44,18 +44,18 @@ export function UserSortBy() {
   return (
     <div className="relative flex justify-center">
       <Listbox value={selectedOption} onChange={changeOption}>
-        <Listbox.Button className="p-2 text-left cursor-pointer border hover:bg-opacity-10 hover:bg-white">
-          <div className="flex w-full">
+        <Listbox.Button className="border-none p-2 hover:ring-4 hover:ring-theme-3 hover:ring-opacity-40 focus:ring-1 focus:ring-theme-3 rounded-xl shadow-theme-shadow-1 focus:shadow-theme-shadow-1">
+          <div className="flex w-full items-center">
             {selectedOption ? selectedOption.name : "Sort order"}
             <FontAwesomeIcon icon={faChevronDown} className="pl-2 w-4 h-4" />
           </div>
         </Listbox.Button>
-        <Listbox.Options className="absolute left-0 p-2 border bg-black top-12 ">
+        <Listbox.Options className="absolute left-0 p-2 border-none top-12 rounded-xl shadow-theme-shadow-1 focus:shadow-theme-shadow-1 bg-white">
           {options.map((person) => (
             <Listbox.Option
               key={person.id}
               value={person}
-              className="px-3 my-1 py-1 cursor-pointer w-72 ui-active:bg-white ui-active:bg-opacity-10 ui-active:text-white whitespace-nowrap"
+              className="px-3 my-1 py-1 cursor-pointer w-72 ui-active:bg-theme-3 ui-active:bg-opacity-10 whitespace-nowrap"
             >
               {person.name}
               <FontAwesomeIcon
