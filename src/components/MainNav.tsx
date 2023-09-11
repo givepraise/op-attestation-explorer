2fb16e72c45467b67d4886e36e1807ceda58e34e
Kristofer <kristofer@fmckl.se>
2023-09-11 22:09:12 +0200

    Bg styling

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -9,7 +9,7 @@ import { WagmiConfig } from "wagmi";
 
 export function MainNav() {
   return (
-    <div className="flex items-start w-full pt-3 px-7 bg-[url('/header.png')] h-40">
+    <div className="flex items-start w-full pt-3 px-7 bg-header h-40">
       <div className="flex flex-col md:flex-row items-center justify-between w-full px-7">
         <div className="flex items-center gap-5">
           <div className="py-5">
