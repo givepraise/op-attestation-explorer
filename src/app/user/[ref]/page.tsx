b0595c3613283801e652abd1418665e429c2c7eb
Kristofer <kristofer@fmckl.se>
2023-09-22 23:55:40 +0200

    Include SearchAndSort on User page

--- src/app/user/[ref]/page.tsx
+++ src/app/user/[ref]/page.tsx
@@ -1,6 +1,7 @@
 import { AttestationCardAlt } from "../../../components/attestation/AttestationCardAlt";
 import { CopyButton } from "../../../components/CopyButton";
 import { DecodedData } from "../../../eas/types/decoded-data.type";
+import { SearchAndSort } from "../../../components/attestations/SearchAndSort";
 import { UserIcon } from "../../../components/user/UserIcon";
 import { getAddressFromEnsName } from "../../../viem/getAddressFromEnsName";
 import { getAllPraiseUsers } from "../../../praise/getAllPraiseUsers";
@@ -43,66 +44,75 @@ export default async function UserPage({
   const praiseUsername = praiseUser?.username;
 
   return (
-    <div className="flex flex-col items-center w-full gap-5">
-      <div className="flex flex-col items-center w-full p-5 gap-10 bg-dots sm:flex-row rounded-xl shadow-theme-shadow-1">
-        <UserIcon address={address} variant="square" size="large" />
-        <div className="flex flex-col items-center sm:items-start gap-1 whitespace-nowrap">
-          {username && <div className="text-xl font-semibold">{username}</div>}
-          <div className="flex items-center gap-1">
-            <a
-              href={`https://optimism.easscan.org/address/${address}`}
-              target="_blank"
-            >
-              {shortenEthAddress(address)}
-            </a>
-            <CopyButton textToCopy={address} />
+    <>
+      <SearchAndSort />
+      <div className="w-full border-b-4 border-theme-gray-1">
+        <div className="text-2xl font-semibold">User</div>
+      </div>
+
+      <div className="flex flex-col items-center w-full gap-5">
+        <div className="flex flex-col items-center w-full p-5 gap-10 bg-dots sm:flex-row rounded-xl shadow-theme-shadow-1">
+          <UserIcon address={address} variant="square" size="large" />
+          <div className="flex flex-col items-center sm:items-start gap-1 whitespace-nowrap">
+            {username && (
+              <div className="text-xl font-semibold">{username}</div>
+            )}
+            <div className="flex items-center gap-1">
+              <a
+                href={`https://optimism.easscan.org/address/${address}`}
+                target="_blank"
+              >
+                {shortenEthAddress(address)}
+              </a>
+              <CopyButton textToCopy={address} />
+            </div>
+          </div>
+          <div className="flex-grow hidden md:flex"></div>
+          <div className="flex flex-col items-center gap-3">
+            {optimistName && (
+              <>
+                <div className="flex flex-row items-baseline sm:flex-col md:items-start gap-3 sm:gap-1">
+                  <div className="text-xs text-gray-500 ">
+                    Optimist Profile Name
+                  </div>
+                  {optimistName}
+                </div>
+                <div className="flex flex-row items-baseline sm:flex-col md:items-start gap-3 sm:gap-1">
+                  <div className="text-xs text-gray-500 ">
+                    Optimist Profile Name
+                  </div>
+                  {optimistName}
+                </div>
+              </>
+            )}
+            {praiseUsername && (
+              <>
+                <div className="text-xs text-gray-500">Praise Username</div>
+                {praiseUsername}
+              </>
+            )}
+          </div>
+          <div className="flex-grow hidden md:flex"></div>
+          <div className="flex items-center justify-between w-40 px-3 py-1 bg-theme-gray-1 rounded-xl">
+            <span className="text-sm">Attestations</span>{" "}
+            <span className="text-xl font-semibold">{attestations.length}</span>
           </div>
         </div>
-        <div className="flex-grow hidden md:flex"></div>
-        <div className="flex flex-col items-center gap-3">
-          {optimistName && (
-            <>
-              <div className="flex flex-row items-baseline sm:flex-col md:items-start gap-3 sm:gap-1">
-                <div className="text-xs text-gray-500 ">
-                  Optimist Profile Name
-                </div>
-                {optimistName}
-              </div>
-              <div className="flex flex-row items-baseline sm:flex-col md:items-start gap-3 sm:gap-1">
-                <div className="text-xs text-gray-500 ">
-                  Optimist Profile Name
-                </div>
-                {optimistName}
-              </div>
-            </>
-          )}
-          {praiseUsername && (
-            <>
-              <div className="text-xs text-gray-500">Praise Username</div>
-              {praiseUsername}
-            </>
-          )}
+        <div className="w-full border-b-4 border-theme-gray-1">
+          <div className="text-2xl font-semibold">Attestations</div>
         </div>
-        <div className="flex-grow hidden md:flex"></div>
-        <div className="flex items-center justify-between w-40 px-3 py-1 bg-theme-gray-1 rounded-xl">
-          <span className="text-sm">Attestations</span>{" "}
-          <span className="text-xl font-semibold">{attestations.length}</span>
-        </div>
-      </div>
-      <div className="w-full border-b-4 border-theme-gray-1">
-        <div className="text-2xl font-semibold">Attestations</div>
-      </div>
-      {attestations.length === 0 && <div>No attestations found</div>}
+        {attestations.length === 0 && <div>No attestations found</div>}
 
-      {attestations.length > 0 && (
-        <ol className="w-full">
-          {attestations.map((att) => (
-            <li key={att.id} className="pb-5">
-              <AttestationCardAlt attestation={att} />
-            </li>
-          ))}
-        </ol>
-      )}
-    </div>
+        {attestations.length > 0 && (
+          <ol className="w-full">
+            {attestations.map((att) => (
+              <li key={att.id} className="pb-5">
+                <AttestationCardAlt attestation={att} />
+              </li>
+            ))}
+          </ol>
+        )}
+      </div>
+    </>
   );
 }
