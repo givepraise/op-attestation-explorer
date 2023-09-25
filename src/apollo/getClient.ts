fd6cdae7f1019174e2a0dda58d91467021a9b88e
Kristofer <kristofer@fmckl.se>
2023-09-25 11:23:11 +0200

    Upgrade Apollo

--- src/apollo/getClient.ts
+++ src/apollo/getClient.ts
@@ -1,15 +1,11 @@
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { DEFAULT_REVALIDATE_TIME, EAS_API_URL } from "../config";
-import {
-  NextSSRApolloClient,
-  NextSSRInMemoryCache,
-} from "@apollo/experimental-nextjs-app-support/ssr";
 
-import { HttpLink } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
 export const { getClient } = registerApolloClient(() => {
-  return new NextSSRApolloClient({
-    cache: new NextSSRInMemoryCache({}),
+  return new ApolloClient({
+    cache: new InMemoryCache(),
     link: new HttpLink({
       uri: EAS_API_URL,
       fetchOptions: { next: { revalidate: DEFAULT_REVALIDATE_TIME } },
