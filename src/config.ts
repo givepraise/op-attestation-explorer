c0b9bfbdc49c7366cb212e54ea87b4cb85a32126
Ramin <raminramazanpour@gmail.com>
2024-10-12 19:12:26 +0330

    add Metrics Garden, DeVouch and Coordinape

--- src/config.ts
+++ src/config.ts
@@ -8,14 +8,19 @@ import RegenScore from "./components/images/regenscore.svg";
 import { RegenScoreCustomDisplay } from "./components/attestation/custom-displays/RegenScoreCustomDisplay";
 import { SchemaListItem } from "./eas/types/schema-list-item.type";
 
-export const EAS_API_URL = "https://optimism.easscan.org/graphql";
+export enum chains {
+  OP = "optimism",
+  BASE = "base",
+}
+
+export const getEASApiUrl = (chain?: chains) => `https://${chain || chains.OP}.easscan.org/graphql`;
 
 export const PRAISE_API_URL = "https://gnosisdao.givepraise.xyz/api/users";
 
 export const USERS_PER_PAGE = 15;
 export const ATTESTATIONS_PER_PAGE = 15;
 
-export const DEFAULT_REVALIDATE_TIME = 60 * 15; // 15 minutes
+export const DEFAULT_REVALIDATE_TIME = 1; // 15 minutes
 
 // The schema IDs for the EAS schemas
 export const UID_OPTIMIST =
@@ -26,6 +31,11 @@ export const UID_REGEN_SCORE =
   "0xa1285d8c9b3164eb94f22a4084d4d01fc7fb66d27c56ddba32033c63a5ed76cd";
 export const UID_PASSPORT_SCORE =
   "0x6ab5d34260fca0cfcf0e76e96d439cace6aa7c3c019d7c4580ed52c6845e9c89";
+export const UID_PRAISE_FARCASTER = '0x82c2ec8ec89cf1d13022ff0867744f1cecf932faa4fe334aa1bb443edbfee3fa';
+export const UID_DEVOUCH = '0x97b0c9911936fad57e77857fac6eef6771f8d0bf025be9549214e32bf9e2415a';
+export const UID_METRICS_GARDEN = '0xc9bc703e3c48be23c1c09e2f58b2b6657e42d8794d2008e3738b4ab0e2a3a8b6';
+export const PRAISE_FARCASTER_CONTRACT = '0xAbdF10bBbccEf43942dc8A1DA8Ae27ddDA1D47d8'
+export const COORDINAPE_CONTRACT = '0x7e823AE179592525301ceb33b3eC479f8c66ecB9'
 
 // The Safe address that is used to attest to Praise attestations
 export const OP_PRAISE_SAFE_ADDRESS =
@@ -34,6 +44,7 @@ export const OP_PRAISE_SAFE_ADDRESS =
 export const EAS_SCHEMAS: SchemaListItem[] = [
   {
     name: "Praise",
+    slug: "praise",
     description:
       "Praise is a community intelligence system that promotes active participation and collaboration through peer recognition and rewards. We help communities become more intelligent, productive and inclusive by providing a simple way for community members to acknowledge, praise and reward each other’s contributions.",
     projectUrl: "https://givepraise.xyz/",
@@ -55,6 +66,7 @@ export const EAS_SCHEMAS: SchemaListItem[] = [
   },
   {
     name: "Regen Score",
+    slug: "regen-score",
     description: "On-chain reputation layer for regens in public goods.",
     projectUrl: "https://regenscore.io/",
     uid: UID_REGEN_SCORE,
@@ -71,6 +83,7 @@ export const EAS_SCHEMAS: SchemaListItem[] = [
   },
   {
     name: "Optimist",
+    slug: "optimist",
     description:
       "Retroactive Public Goods Funding is moving towards a future in which the Collective gives projects the ability to quantify their impact. The Optimist Profile is a first step towards this future, enabling projects to self-report their impact & profit and provide references to relevant data sources.",
     projectUrl: "https://gov.optimism.io/",
@@ -85,6 +98,7 @@ export const EAS_SCHEMAS: SchemaListItem[] = [
   },
   {
     name: "Passport Score",
+    slug: "passport-score",
     description:
       "By collecting “stamps” of validation for your identity and online reputation, you can gain access to the most trustworthy web3 experiences and maximize your ability to benefit from platforms like Gitcoin Grants. The more you verify your identity, the more opportunities you will have to vote and participate across the web3.",
     projectUrl: "https://passport.gitcoin.co",
@@ -94,6 +108,72 @@ export const EAS_SCHEMAS: SchemaListItem[] = [
     gqlWhere: {
       schemaId: {
         equals: UID_PASSPORT_SCORE,
+      },
+    },
+  },
+  {
+    name: "Praise on Farcaster",
+    slug: "praise-on-farcaster",
+    description:
+        "Praise is a community intelligence system that promotes active participation and collaboration through peer recognition and rewards. We help communities become more intelligent, productive and inclusive by providing a simple way for community members to acknowledge, praise and reward each other’s contributions.",
+    projectUrl: "https://givepraise.xyz/",
+    uid: UID_PRAISE_FARCASTER,
+    displayComponent: PassportCustomDisplay,
+    logo: Passport,
+    gqlWhere: {
+      schemaId: {
+        equals: UID_PRAISE_FARCASTER,
+      },
+      attester: {
+        equals: PRAISE_FARCASTER_CONTRACT,
+      }
+    },
+    chain: chains.BASE,
+  },
+  {
+    name: "Coordinape",
+    slug: "coordinape",
+    description:
+        "Coordinape is a decentralized, trustless, and permissionless protocol for distributing funds and reputation in a community. It is designed to be a simple, transparent, and fair way to distribute funds and reputation in a community.",
+    projectUrl: "https://coordinape.com/",
+    uid: UID_PRAISE_FARCASTER,
+    displayComponent: PassportCustomDisplay,
+    logo: Passport,
+    gqlWhere: {
+      schemaId: {
+        equals: UID_PRAISE_FARCASTER,
+      },
+      attester: {
+        equals: COORDINAPE_CONTRACT,
+      }
+    },
+    chain: chains.BASE,
+  },
+  {
+    name: "DeVouch",
+    slug: "devouch",
+    description: "DeVouch is a decentralized, trustless, and permissionless protocol for distributing funds and reputation in a community. It is designed to be a simple, transparent, and fair way to distribute funds and reputation in a community.",
+    projectUrl: "https://coordinape.com/",
+    uid: UID_DEVOUCH,
+    displayComponent: PassportCustomDisplay,
+    logo: Passport,
+    gqlWhere: {
+      schemaId: {
+        equals: UID_DEVOUCH,
+      },
+    },
+  },
+  {
+    name: "Metrics Garden",
+    slug: "metrics-garden",
+    description: "Metrics Garden is a decentralized, trustless, and permissionless protocol for distributing funds and reputation in a community. It is designed to be a simple, transparent, and fair way to distribute funds and reputation in a community.",
+    projectUrl: "https://www.metricsgarden.xyz/",
+    uid: UID_METRICS_GARDEN,
+    displayComponent: PassportCustomDisplay,
+    logo: Passport,
+    gqlWhere: {
+      schemaId: {
+        equals: UID_METRICS_GARDEN,
       },
     },
   },
